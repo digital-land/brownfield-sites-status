cd6a0b01cdce659f05114d0cd1ff1b98d697d901
Colm Britton <colmjude@gmail.com>
2019-06-25 08:39:06 +0100

    Update filter to output id if mapping not available

--- application/filters.py
+++ application/filters.py
@@ -2,4 +2,6 @@ from application.data import LocalAuthorityMapping
 la_mapping = LocalAuthorityMapping()
 
 def map_la_code_to_name(id):
-	return la_mapping.get_local_authority_name(id)
+	if la_mapping.get_local_authority_name(id) is not None:
+		return la_mapping.get_local_authority_name(id)
+	return id
