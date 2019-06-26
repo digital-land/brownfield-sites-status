72d8f2144978ee3e811120ff1bebeb1842fd013e
Colm Britton <colmjude@gmail.com>
2019-06-26 06:37:40 +0100

    Summarise results

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -17,15 +17,39 @@ def fetch_results(url):
     except requests.ConnectionError:
         return "Connection Error"
 
-    data = resp.json()
+    return resp.json()
+
+
+def fetch_sorted_results(url):
+    data = fetch_results(url)
     items = data['Items']
     items.sort(key=lambda x: x['organisation'])
     return items
 
 
+def summarise_results(url):
+    summary = {"total": 0, "url": 0, "csv": 0, "headers": 0, "valid": 0}
+    data = fetch_results(url)
+    for i in data['Items']:
+        summary['total'] += 1
+        if i['validated']['statusCode'] is 200:
+            summary['url'] += 1
+        if i['validated']['isCsv'] is True:
+            summary['csv'] += 1
+        if i['validated']['hasRequiredHeaders'] is True:
+            summary['headers'] += 1
+        if i['validated']['isValid'] is True:
+            summary['valid'] += 1
+    return summary
+
 @frontend.route('/')
 def index():
-    return render_template('index.html', data=fetch_results(current_app.config['STATUS_API']))
+    return render_template('index.html', data=fetch_sorted_results(current_app.config['STATUS_API']))
+
+
+@frontend.route('/overview')
+def overview():
+    return render_template('overview.html', data=summarise_results(current_app.config['STATUS_API']))
 
 
 # set the assetPath variable for use in
