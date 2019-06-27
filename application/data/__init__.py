277ae3485f35c2dd425c892d8a210e2ba3681d26
Adam Shimali <adam.shimali@gmail.com>
2019-06-27 15:38:45 +0100

    A bit of refactoring and added mapping for national park names.

--- application/data/__init__.py
+++ application/data/__init__.py
@@ -17,5 +17,16 @@ class LocalAuthorityMapping:
                 key = f"local-authority-eng:{row['local-authority-eng']}"
                 self.local_authority_mapping[key] = row['official-name']
 
+        self.local_authority_mapping['national-park:1'] = 'Peak District National Park'
+        self.local_authority_mapping['national-park:2'] = 'Lake District National Park'
+        self.local_authority_mapping['national-park:3'] = 'Dartmoor National Park'
+        self.local_authority_mapping['national-park:4'] = 'North York Moors National Park'
+        self.local_authority_mapping['national-park:5'] = 'Yorkshire Dales National Park'
+        self.local_authority_mapping['national-park:6'] = 'Exmoor National Park'
+        self.local_authority_mapping['national-park:7'] = 'Northumberland National Park'
+        self.local_authority_mapping['national-park:8'] = 'Broads Authority'
+        self.local_authority_mapping['national-park:9'] = 'New Forest National Park'
+        self.local_authority_mapping['national-park:10'] = 'South Downs National Park'
+
     def get_local_authority_name(self, local_authority_id):
         return self.local_authority_mapping.get(local_authority_id)
---
+++ application/frontend/utils.py
@@ -0,0 +1,69 @@
+import requests
+
+from datetime import datetime
+from flask import current_app
+from sqlalchemy.orm.exc import NoResultFound
+from application.extensions import db
+from application.models import Cache
+
+
+def get_cached_result(url, for_date=None):
+    if for_date is not None:
+        return db.session.query(Cache).filter(Cache.url == url, Cache.created_date == for_date).one()
+    else:
+        return db.session.query(Cache).filter(Cache.url == url).order_by(Cache.created_date).one()
+
+
+def cache_result(url, data):
+    if db.session.query(Cache).filter(Cache.url == url, Cache.created_date == datetime.today().date()).first() is not None:
+        current_app.logger.info('Already have cached data for today')
+    else:
+        try:
+            cache = Cache(url=url, data=data)
+            db.session.add(cache)
+            db.session.commit()
+        except Exception as e:
+            current_app.logger.exception('Could not cache results')
+
+
+def fetch_results(url):
+
+    try:
+        cache = get_cached_result(url, for_date=datetime.today().date())
+        data = cache.data
+    except NoResultFound as e:
+        current_app.logger.exception(e)
+        current_app.logger.info('No cached results')
+
+    try:
+        resp = requests.get(url)
+        resp.raise_for_status()
+        data = resp.json()
+        cache_result(url, data)
+
+    except Exception as e:
+        data = get_cached_result(url)
+
+    return data
+
+
+def sort_results(data):
+    data['Items'].sort(key=lambda x: x['organisation'])
+    return data
+
+
+def summarise_results(url):
+    summary = {"total": 0, "url": 0, "csv": 0, "headers": 0, "valid": 0}
+    data = fetch_results(url)
+    for i in data['Items']:
+        if i['validated'] is not None:
+            summary['total'] += 1
+            if i['validated']['statusCode'] is 200:
+                summary['url'] += 1
+            if i['validated']['isCsv'] is True:
+                summary['csv'] += 1
+            if i['validated']['hasRequiredHeaders'] is True:
+                summary['headers'] += 1
+            if i['validated']['isValid'] is True:
+                summary['valid'] += 1
+    return {"last_updated": data['last_updated'], "summary": summary}
