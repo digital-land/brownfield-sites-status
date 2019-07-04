c0ec14135f35a8451388ec7111f5300b7b309318
Colm Britton <colmjude@gmail.com>
2019-07-04 08:19:37 +0100

    Flatten errors if empty, yes and anyOf encountered for same field

--- application/frontend/utils.py
+++ application/frontend/utils.py
@@ -44,11 +44,64 @@ def fetch_results(url):
         return {}
 
 
+def make_empty_or_yes_error(field_name):
+    return {
+        'preprocessed': True,
+        'dataPath': field_name,
+        'message': 'should be either empty or <code>yes</code>. Is case senstive.'
+    }
+
+
+def check_for_empty_and_yes_type(row):
+    looking_for = [
+        ('maxLength', 'should NOT be longer than 0 characters'),
+        ('pattern', 'should match pattern "(yes)"'),
+        ('anyOf', 'should match some schema in anyOf')]
+    lastField = ""
+
+    error_idxs = []
+    summary = dict()
+
+    for idx, error in enumerate(row['validator']['rowErrors']):
+        currentField = error['dataPath'].strip('.')
+
+        if currentField not in summary:
+            summary[currentField] = []
+
+        if (error['keyword'], error['message']) in looking_for:
+            summary[currentField].append(idx)
+
+    new_error_list = []
+    to_remove = []
+    for field in summary.keys():
+        if len(summary[field]) == 3:
+            to_remove = to_remove + summary[field]
+            new_error_list.append(make_empty_or_yes_error(field))
+
+    errors_to_keep = list(set(range(len(row['validator']['rowErrors']))) - set(to_remove))
+    for idx in errors_to_keep:
+        new_error_list.append(row['validator']['rowErrors'][idx])
+    
+    row['validator']['rowErrors'] = new_error_list
+
+    return row
+
+
+def preprocess_validation_results(data):
+    data['_preprocessed'] = False
+    if len(data['latestValidationResult']) > 0:
+        data['_preprocessed'] = True
+        for idx, row in enumerate(data['latestValidationResult']):
+            if not row['validator']['isRowValid']:
+                data['latestValidationResult'][idx] = check_for_empty_and_yes_type(row)
+    return data
+
+
 def fetch_validation_result(url):
     try:
         resp = requests.get(url)
         data = resp.json()
-        return data
+        return preprocess_validation_results(data)
     except Exception as e:
         print(e)
         return {}
