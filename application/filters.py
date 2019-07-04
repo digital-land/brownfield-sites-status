01fe8cd3acd4bfd2893895151582e316212b69a7
Colm Britton <colmjude@gmail.com>
2019-07-04 08:41:42 +0100

    Mapping for PlanningStatus field

--- application/filters.py
+++ application/filters.py
@@ -15,6 +15,11 @@ def strip(s, chars):
 def display_error_message(error):
 	if 'Date' in error['dataPath'] and error['keyword'] == 'pattern':
 		return "should be in format YYYY-MM-DD"
+
 	if 'OwnershipStatus' in error['dataPath'] and error['keyword'] == 'pattern':
 		return "should be one of <code>owned by a public authority</code>, <code>not owned by a public authority</code>, <code>unknown ownership</code> or <code>mixed ownership</code>"
+
+	if 'PlanningStatus' in error['dataPath'] and error['keyword'] == 'pattern':
+		return "should be one of <code>permissioned</code>, <code>not permissioned</code> or <code>pending decision</code>"
+
 	return error['message']
